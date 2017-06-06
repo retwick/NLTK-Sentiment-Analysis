2f118db35f6de34014433218954863bca4dfdef5
Retwick Rajeevan <retwickrajeevan1@gmail.com>
2017-06-07 00:57:35 +0530

    movie reviews

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
 import nltk
-#nltk.download('punkt')
-#nltk.download('stopwords')
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
+import random
+from nltk.corpus import movie_reviews
 
+documents = [(list(movie_reviews.words(fileid)), category)
+             for category in movie_reviews.categories()
+             for fileid in movie_reviews.fileids(category)]
 
-EXAMPLE_TEXT = "Hello Mr. Smith, how are you doing today? The weather is great, and Python is awesome. The sky is pinkish-blue. You shouldn't eat cardboard."
-stemmer_text  = "i missed you missing me colour coloured and colouring all the walls"
+random.shuffle(documents)
 
+all_words = []
+for w in movie_reviews.words():
+    all_words.append(w.lower())
 
-stop_words = set(stopwords.words('english'))
-word_tokens = word_tokenize(EXAMPLE_TEXT)
-ps = PorterStemmer()
+all_words = nltk.FreqDist(all_words)
 
-#filtered_sentence = [w for w in word_tokens if not w in stop_words]
-
-filtered_sentence = []
-
-for w in word_tokens:
-    if w not in stop_words:
-        filtered_sentence.append(w)
-print("WORD TOKENS")
-print(word_tokens)
-print("FILTERED SENTENCE")
-print(filtered_sentence)
-for w in word_tokens:
-	print(ps.stem(w))
-
-#for x in filtered_sentence:
-#	print(x) 
-
-#print(sent_tokenize(EXAMPLE_TEXT))
+word_features = list(all_words.keys())[:3000]
