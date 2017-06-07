8a54e6ef608d26c2d9320ee4d42395e06d9b0905
Retwick Rajeevan <retwickrajeevan1@gmail.com>
2017-06-07 15:00:00 +0530

    trying different classifiers

--- save_classif.py
+++ save_classif.py
@@ -1,8 +1,15 @@
 import nltk
 import random
 import pickle
+import sklearn
+import time
 from nltk.corpus import movie_reviews
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.naive_bayes import MultinomialNB,BernoulliNB
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
 
+start_time = time.time()
 documents = [(list(movie_reviews.words(fileid)), category)
              for category in movie_reviews.categories()
              for fileid in movie_reviews.fileids(category)]
@@ -34,16 +41,46 @@ def find_features(document):
 featuresets = [(find_features(rev), category) for (rev, category) in documents]
 
 # set that we'll train our classifier with
-training_set = featuresets[:1200]
+training_set = featuresets[:1900]
+
+testing_set = featuresets[1900:]
 
 ##########################################################
 ###  CHOOSING A CLASSIFIER  ##############################
 ##########################################################
 
 classifier = nltk.NaiveBayesClassifier.train(training_set)
+print('NB Classifier accuracy percent:',(nltk.classify.accuracy(classifier, testing_set)))
 
-#SAVING THE CLASSIFIER
+MNB_classifier = SklearnClassifier(MultinomialNB())
+MNB_classifier.train(training_set)
+print("MultinomialNB accuracy percent:",nltk.classify.accuracy(MNB_classifier, testing_set))
 
-save_classifier = open("NB.pickle","wb")
-pickle.dump(classifier, save_classifier)
-save_classifier.close()
+BNB_classifier = SklearnClassifier(BernoulliNB())
+BNB_classifier.train(training_set)
+print("BernoulliNB accuracy percent:",nltk.classify.accuracy(BNB_classifier, testing_set))
+
+print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set)))
+
+LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+LogisticRegression_classifier.train(training_set)
+print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set)))
+
+SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+SGDClassifier_classifier.train(training_set)
+print("SGDClassifier_classifier accuracy percent:", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set)))
+
+SVC_classifier = SklearnClassifier(SVC())
+SVC_classifier.train(training_set)
+print("SVC_classifier accuracy percent:", (nltk.classify.accuracy(SVC_classifier, testing_set)))
+
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(training_set)
+print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set)))
+
+NuSVC_classifier = SklearnClassifier(NuSVC())
+NuSVC_classifier.train(training_set)
+print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set)))
+
+print("Excecution time = %s" %(time.time() - start_time))
+
